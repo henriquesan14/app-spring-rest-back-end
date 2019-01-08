faee16aa7e5b64bc4c6d2b32373c2471fc17f77b
Henrique <ricogx@hotmail.com>
2019-01-07 23:23:46 -0300

    relacionamento muitos pra muitos, categorias e produtos

--- src/main/java/apirest/App.java
+++ src/main/java/apirest/App.java
@@ -1,12 +1,46 @@
 package apirest;
 
+import apirest.domain.Categoria;
+import apirest.domain.Produto;
+import apirest.repositories.CategoriaRepository;
+import apirest.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Produto p1= new Produto(null,"Computador",2000.00);
+        Produto p2=new Produto(null, "Impressora", 800.00);
+        Produto p3=new Produto(null, "Mouse", 80.00);
+        Categoria cat1 = new Categoria(null, "Informatica");
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+        Categoria cat2 = new Categoria(null, "Escritorio");
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+        produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
     }
 }
--- src/main/java/apirest/domain/Produto.java
+++ src/main/java/apirest/domain/Produto.java
@@ -4,9 +4,10 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
     private static final long serialVersionUID= 1L;
 
     @Id
@@ -15,15 +16,21 @@ public class Categoria implements Serializable {
 
     private String nome;
 
-    @ManyToMany(mappedBy = "categorias")
-    private List<Produto> produtos = new ArrayList<>();
+    private Double preco;
 
-    public Categoria() {
+    @ManyToMany
+    @JoinTable(name = "PRODUTO_CATEGORIA",
+    joinColumns = @JoinColumn(name="produto_id"),
+    inverseJoinColumns = @JoinColumn(name="categoria_id"))
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public Produto() {
     }
 
-    public Categoria(Integer id, String nome) {
+    public Produto(Integer id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
+        this.preco = preco;
     }
 
     public Integer getId() {
@@ -42,12 +49,20 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
-    public List<Produto> getProdutos() {
-        return produtos;
+    public Double getPreco() {
+        return preco;
     }
 
-    public void setProdutos(List<Produto> produtos) {
-        this.produtos = produtos;
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
     }
 
     @Override
@@ -66,7 +81,7 @@ public class Categoria implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Categoria other = (Categoria) obj;
+        Produto other = (Produto) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
