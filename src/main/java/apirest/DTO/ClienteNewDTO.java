51f87ec30d11ae577a154da158460a8bf18be433
Henrique <ricogx@hotmail.com>
2019-01-09 19:42:41 -0300

    validação customizada de CPF ou CNPJ na inserção de cliente

--- src/main/java/apirest/DTO/ClienteNewDTO.java
+++ src/main/java/apirest/DTO/ClienteNewDTO.java
@@ -1,22 +1,45 @@
 package apirest.DTO;
 
+import apirest.services.validation.ClienteInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Email(message = "Email inválido")
     private String email;
+    
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String cpfOuCnpj;
+
     private Integer tipoCliente;
 
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String logradouro;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String numero;
+
     private String complemento;
+
     private String bairro;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String cep;
 
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String telefone1;
+
     private String telefone2;
     private String telefone3;
 
