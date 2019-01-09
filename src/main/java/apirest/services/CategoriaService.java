ded1fded0edc04ec3de2a4700bdb404abd2a3295
Henrique <ricogx@hotmail.com>
2019-01-09 17:32:23 -0300

    corrções: update em categoriaService e construtor Pagamento

--- src/main/java/apirest/services/CategoriaService.java
+++ src/main/java/apirest/services/CategoriaService.java
@@ -37,8 +37,9 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj){
-        find(obj.getId());
-        return categorias.save(obj);
+        Categoria newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return categorias.save(newObj);
     }
 
     public void delete(Integer id){
@@ -58,5 +59,9 @@ public class CategoriaService {
 
     public Categoria fromDto(CategoriaDTO objDto){
         return new Categoria(objDto.getId(),objDto.getNome());
+    }
+
+    private void updateData(Categoria newObj, Categoria obj){
+        newObj.setNome(obj.getNome());
     }
 }
