58ec11192617399bee8c4e8cf2cfb5ba5d063a59
Henrique <ricogx@hotmail.com>
2019-01-10 00:24:31 -0300

    inserindo pedido

--- src/main/java/apirest/resources/PedidoResource.java
+++ src/main/java/apirest/resources/PedidoResource.java
@@ -5,7 +5,10 @@ import apirest.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,5 +27,13 @@ public class PedidoResource {
     public ResponseEntity<Pedido> find(@PathVariable Integer id) {
         Pedido obj = pedidoService.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+        obj = pedidoService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/apirest/services/PedidoService.java
+++ src/main/java/apirest/services/PedidoService.java
@@ -1,11 +1,18 @@
 package apirest.services;
 
+import apirest.domain.ItemPedido;
+import apirest.domain.PagamentoComBoleto;
 import apirest.domain.Pedido;
+import apirest.domain.enums.EstadoPagamento;
+import apirest.repositories.ItemPedidoRepository;
+import apirest.repositories.PagamentoRepository;
 import apirest.repositories.PedidoRepository;
 import apirest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +22,18 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidos;
 
+    @Autowired
+    private BoletoService boletoService;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
     public Pedido find(Integer id) {
         Optional<Pedido> obj = pedidos.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -23,5 +42,26 @@ public class PedidoService {
 
     public List<Pedido> findAll(){
         return pedidos.findAll();
+    }
+
+    @Transactional
+    public Pedido insert(Pedido obj){
+        obj.setId(null);
+        obj.setInstante(new Date());
+        obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+        obj.getPagamento().setPedido(obj);
+        if(obj.getPagamento() instanceof PagamentoComBoleto){
+            PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+            boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
+        }
+        obj = pedidos.save(obj);
+        pagamentoRepository.save(obj.getPagamento());
+        for(ItemPedido ip: obj.getItens()){
+            ip.setDesconto(0.0);
+            ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+            ip.setPedido(obj);
+        }
+        itemPedidoRepository.saveAll(obj.getItens());
+        return obj;
     }
 }
