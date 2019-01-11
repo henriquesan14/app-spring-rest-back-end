2f385c0eb7c0bc343b89a59a73da22261202ef30
Henrique <ricogx@hotmail.com>
2019-01-10 22:07:57 -0300

    impl envio de email HTML

--- src/main/java/apirest/services/EmailService.java
+++ src/main/java/apirest/services/EmailService.java
@@ -3,9 +3,14 @@ package apirest.services;
 import apirest.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido pedido);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
