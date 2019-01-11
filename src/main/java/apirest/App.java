65ecf6545404da8bd65565f8e6181c775de9217e
Henrique <ricogx@hotmail.com>
2019-01-10 22:46:53 -0300

    config inicial spring security

--- src/main/java/apirest/App.java
+++ src/main/java/apirest/App.java
@@ -1,16 +1,8 @@
 package apirest;
 
-import apirest.domain.*;
-import apirest.domain.enums.EstadoPagamento;
-import apirest.domain.enums.TipoCliente;
-import apirest.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class App implements CommandLineRunner {
