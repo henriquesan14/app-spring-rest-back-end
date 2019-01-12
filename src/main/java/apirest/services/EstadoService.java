d5ad193b52bb3074c183a335a4196ce89d6e46fa
Henrique <ricogx@hotmail.com>
2019-01-11 22:22:06 -0300

    endpoints busca estados e cidades

---
+++ src/main/java/apirest/services/EstadoService.java
@@ -0,0 +1,20 @@
+package apirest.services;
+
+import apirest.domain.Estado;
+import apirest.repositories.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> findAll(){
+        return estadoRepository.findAllByOrderByNome();
+    }
+}
+
